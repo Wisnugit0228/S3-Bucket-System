ccf60ff6970f2867a19372560f1c0aac1ccb27e1
Wisnugit0228 <dwicwisnu0228@gmail.com>
2026-01-22 10:02:13 +0700

    fix index.js

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -11,7 +11,26 @@ const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.status(200).json({
+    status: "success",
+    endpoint: {
+      uploadFile: {
+        url: "/uploads",
+        method: "POST",
+        body: {
+          dir: "string",
+          file: "file",
+        },
+      },
+      dowloadFile: {
+        url: "/download",
+        method: "GET",
+        query: {
+          key: "string",
+        },
+      },
+    },
+  });
 });
 
 router.post("/uploads", upload.single("file"), uploadFileController);
